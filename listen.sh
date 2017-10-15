6a198eb67a09310ed5c8ea1da5740b2df3024c23
Rocky Li <zl131@duke.edu>
2017-10-15 15:22:58 -0400

    wifi listener script on raspberry pi

---
+++ listen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ 
+# Sleep to wait for system ready
+
+sleep 10
+filename="/home/pi/workspace/wiflistener/records.log"
+echo "Starting script at $(date)" >> $filename
+wpa_cli scan
+
+loop=99999
+
+if [[ $# -ge 1 ]]
+then
+	echo "doing $1 loops"
+	loop=$1
+else
+	echo "doing infinite loops"
+fi
+
+num=1
+
+while [[ $num -le $loop ]]
+do
+	echo $num
+	echo "ITERATION $num" >> $filename
+	date >> $filename
+	wpa_cli scan_results | sed '/Selected/,+1 d' >> $filename
+	echo "END ITERATION $num" >> $filename
+	sleep 5
+	((num++))
+done
