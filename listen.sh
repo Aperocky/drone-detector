21725a9b1e6281ca3812fefeb02aae3596a124b8
Rocky Li <zl131@duke.edu>
2017-10-15 16:24:04 -0400

    Modified sleep times

--- listen.sh
+++ listen.sh
@@ -2,19 +2,20 @@
  
 # Sleep to wait for system ready
 
-sleep 60
+sleep 30
 filename="/home/pi/workspace/wiflistener/records.log"
 echo "Starting script at $(date)" >> $filename
 wpa_cli scan >> $filename
+sleep 5
 
 loop=99999
 
 if [[ $# -ge 1 ]]
 then
-	echo "doing $1 loops"
+	echo "doing $1 loops" >> $filename
 	loop=$1
 else
-	echo "doing infinite loops"
+	echo "doing infinite loops" >> $filename
 fi
 
 num=1
